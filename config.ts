5ce6589fcdc76881d2322d154bd9d21184bf59ba
Duc Minh Le <122418348+minhducle25@users.noreply.github.com>
2025-11-11 00:10:23 +1100

    dùng zod config đê validate env config

---
+++ config.ts
@@ -0,0 +1,18 @@
+import z, { config } from "zod";
+import { de } from "zod/locales";
+
+const configSchema = z.object({
+    NEXT_PUBLIC_API_ENDPOINT: z.string(),
+    NEXT_PULIC_URL: z.string(),
+});
+
+const configProject = configSchema.safeParse({
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    NEXT_PULIC_URL: process.env.NEXT_PULIC_URL,
+})
+if (!configProject.success) {
+    console.error(configProject.error.issues)
+    throw new Error("Invalid client .env configuration");
+}
+const envConfig = configProject.data;
+export default envConfig;
