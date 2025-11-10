371e6adef550645a18c7fb7433a89f6528eb3669
Duc Minh Le <122418348+minhducle25@users.noreply.github.com>
2025-11-10 23:22:19 +1100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <Button>Click me</Button>
+
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
