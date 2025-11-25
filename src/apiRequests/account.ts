2cbdfabda08a467dc83de994538d6f24eb5a53e2
Duc Minh Le <122418348+minhducle25@users.noreply.github.com>
2025-11-25 23:34:59 +1100

    add querries for managing accounts

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -1,9 +1,29 @@
+import AddEmployee from "@/app/manage/accounts/add-employee";
 import http from "@/lib/http";
-import { AccountResType, ChangePasswordBodyType, UpdateMeBodyType } from "@/schemaValidations/account.schema";
+import {
+  AccountListResType,
+  AccountResType,
+  ChangePasswordBodyType,
+  CreateEmployeeAccountBodyType,
+  UpdateEmployeeAccountBodyType,
+  UpdateMeBodyType,
+} from "@/schemaValidations/account.schema";
 
-const accountApiRequest ={
-    me: () => http.get<AccountResType>('/accounts/me'),
-    updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>('/accounts/me', body),
-    changePwd: (body: ChangePasswordBodyType) => http.put<AccountResType>('/accounts/change-password', body)
-}
+const prefix = "/accounts";
+const accountApiRequest = {
+  me: () => http.get<AccountResType>(`${prefix}/me`),
+  updateMe: (body: UpdateMeBodyType) =>
+    http.put<AccountResType>(`${prefix}/me`, body),
+  changePwd: (body: ChangePasswordBodyType) =>
+    http.put<AccountResType>(`${prefix}/change-password`, body),
+  list: () => http.get<AccountListResType>(`${prefix}`),
+  AddEmployee: (body: CreateEmployeeAccountBodyType) =>
+    http.post<AccountResType>(prefix, body),
+  updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) =>
+    http.put<AccountResType>(`${prefix}/detail/${id}`, body),
+  getEmployee: (id: number) =>
+    http.get<AccountResType>(`${prefix}/detail/${id}`),
+  deleteEmployee: (id: number) =>
+    http.delete<AccountResType>(`${prefix}/detail/${id}`),
+};
 export default accountApiRequest;
--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,19 +1,64 @@
-import accountApiRequest from "@/apiRequests/account"
-import { useMutation, useQuery } from "@tanstack/react-query"
+import accountApiRequest from "@/apiRequests/account";
+import { UpdateEmployeeAccountBodyType } from "@/schemaValidations/account.schema";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useAccountMe = () => {
-    return useQuery({
-        queryKey: ["account-profile"],
-        queryFn: accountApiRequest.me
-    })
-}
+  return useQuery({
+    queryKey: ["account-profile"],
+    queryFn: accountApiRequest.me,
+  });
+};
 export const useUpdateMeMutationMutation = () => {
-    return useMutation({
-        mutationFn: accountApiRequest.updateMe
-    })
-}
+  return useMutation({
+    mutationFn: accountApiRequest.updateMe,
+  });
+};
 export const useChangePwdMutation = () => {
-    return useMutation({
-        mutationFn: accountApiRequest.changePwd
-    })
-}
+  return useMutation({
+    mutationFn: accountApiRequest.changePwd,
+  });
+};
+export const useGetAccountList = () => {
+  return useQuery({
+    queryKey: ["accounts"],
+    queryFn: accountApiRequest.list,
+  });
+};
+export const useGetAccount = ({ id }: { id: number }) => {
+  return useQuery({
+    queryKey: ["account", id],
+    queryFn: () => accountApiRequest.getEmployee(id),
+  });
+};
+
+export const useAddAccountMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: accountApiRequest.AddEmployee,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+  });
+};
+export const useUpdateAccountMutation = ({ id }: { id: number }) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({
+      id,
+      ...body
+    }: UpdateEmployeeAccountBodyType & { id: number }) =>
+      accountApiRequest.updateEmployee(id, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+  });
+};
+export const useDeleteAccountMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: accountApiRequest.deleteEmployee,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+  });
+};
