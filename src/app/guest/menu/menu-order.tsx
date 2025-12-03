be73eab3d6e757365c160c17c6c70e9d8026ff1a
Duc Minh Le <122418348+minhducle25@users.noreply.github.com>
2025-12-04 00:38:13 +1100

    added logic to calculate quantity and totalPrice in guest menu

--- src/app/guest/menu/menu-order.tsx
+++ src/app/guest/menu/menu-order.tsx
@@ -1,54 +1,76 @@
-'use client'
-import Image from 'next/image'
-import { Minus, Plus } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import {  useGetDishList } from '@/queries/useDish'
-import { formatCurrency } from '@/lib/utils'
-
+"use client";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useGetDishList } from "@/queries/useDish";
+import { formatCurrency } from "@/lib/utils";
+import Quantity from "@/app/guest/menu/quantity";
+import { useState } from "react";
+import { GuestCreateOrdersBodyType } from "@/schemaValidations/guest.schema";
 
 export default function MenuOrder() {
-  const {data} = useGetDishList()
-  const dishes = data?.payload.data ?? []
-  
-    return (
+  const { data } = useGetDishList();
+  const dishes = data?.payload.data ?? [];
+  const [orders, setOrder] = useState<GuestCreateOrdersBodyType>([]);
+  const totalPrice = dishes.reduce((result, dish) => {
+    const order = orders.find((o) => o.dishId === dish.id);
+    if (!order) return result;
+    return result + order.quantity * dish.price;
+  }, 0);
+
+  const handleQuantityChange = (dishId: number, quantity: number) => {
+    setOrder((prevOrders) => {
+      if (quantity === 0) {
+        return prevOrders.filter((order) => order.dishId !== dishId);
+      }
+
+      const index = prevOrders.findIndex((order) => order.dishId === dishId);
+
+      if (index === -1) {
+        return [...prevOrders, { dishId, quantity }];
+      }
+      const newOrders = [...prevOrders];
+      newOrders[index] = { ...newOrders[index], quantity };
+      return newOrders;
+    });
+  };
+  console.log(orders);
+  return (
     <>
-          {dishes.map((dish) => (
-        <div key={dish.id} className='flex gap-4'>
-          <div className='flex-shrink-0'>
+      {dishes.map((dish) => (
+        <div key={dish.id} className="flex gap-4">
+          <div className="flex-shrink-0">
             <Image
               src={dish.image}
               alt={dish.name}
               height={100}
               width={100}
               quality={100}
-              className='object-cover w-[80px] h-[80px] rounded-md'
+              className="object-cover w-[80px] h-[80px] rounded-md"
             />
           </div>
-          <div className='space-y-1'>
-            <h3 className='text-sm'>{dish.name}</h3>
-            <p className='text-xs'>{dish.description}</p>
-            <p className='text-xs font-semibold'>{formatCurrency(dish.price)}</p>
+          <div className="space-y-1">
+            <h3 className="text-sm">{dish.name}</h3>
+            <p className="text-xs">{dish.description}</p>
+            <p className="text-xs font-semibold">
+              {formatCurrency(dish.price)}
+            </p>
           </div>
-          <div className='flex-shrink-0 ml-auto flex justify-center items-center'>
-            <div className='flex gap-1 '>
-              <Button className='h-6 w-6 p-0'>
-                <Minus className='w-3 h-3' />
-              </Button>
-              <Input type='text' readOnly className='h-6 p-1 w-8' />
-              <Button className='h-6 w-6 p-0'>
-                <Plus className='w-3 h-3' />
-              </Button>
-            </div>
+          <div className="flex-shrink-0 ml-auto flex justify-center items-center">
+            <Quantity
+              onChange={(value) => handleQuantityChange(dish.id, value)}
+              value={
+                orders.find((order) => order.dishId === dish.id)?.quantity ?? 0
+              }
+            />
           </div>
         </div>
       ))}
-      <div className='sticky bottom-0'>
-        <Button className='w-full justify-between'>
-          <span>Giỏ hàng · 2 món</span>
-          <span>100,000 đ</span>
+      <div className="sticky bottom-0">
+        <Button className="w-full justify-between">
+          <span>Giỏ hàng · {orders.length} món</span>
+          <span>{formatCurrency(totalPrice)}</span>
         </Button>
       </div>
-      </>
-  )
+    </>
+  );
 }
